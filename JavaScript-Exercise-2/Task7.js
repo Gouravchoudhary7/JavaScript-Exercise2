587052b7d8ba9e4d0e22674064c48a417c702eb1
Gouravchoudhary7 <97522795+Gouravchoudhary7@users.noreply.github.com>
2022-01-29 22:28:24 +0530

    Delete Task7.js

--- JavaScript-Exercise-2/Task7.js
+++
@@ -1,5 +0,0 @@
-let month_name = function (dat){
-    month_list = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-  return month_list[dat.getMonth()];
-}
-console.log(month_name(new Date("07/01/2020")));
