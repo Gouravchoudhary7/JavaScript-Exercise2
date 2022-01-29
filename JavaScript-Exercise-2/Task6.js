ba2cf289d1eb933cde30c3b2866dab35444b457d
Gouravchoudhary7 <97522795+Gouravchoudhary7@users.noreply.github.com>
2022-01-29 22:28:17 +0530

    Delete Task6.js

--- JavaScript-Exercise-2/Task6.js
+++
@@ -1,9 +0,0 @@
-let static_array = [ "a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z" ];
-
-for (let i = static_array.length - 1; i >= 0; i--) {
-  if (i % 2 == 1)
-    static_array.splice(i, 1);
-}
-
-
-console.log(static_array);
